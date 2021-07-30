ebd23914fa04accc37514d2b5898ea0b3ae3438c
Pedro da Matta <pmrm@cin.ufpe.br>
2021-07-30 09:52:56 -0300

    Add background image to drag

--- EmotionalBombs/EmotionalBombs/AppDelegate.swift
+++ EmotionalBombs/EmotionalBombs/AppDelegate.swift
@@ -14,7 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = DragController() // Your initial view controller.
+        window.makeKeyAndVisible()
+        self.window = window
+
         return true
     }
 
